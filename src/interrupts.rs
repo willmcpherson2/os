d77e1d91beeaa6f50d63238940ae25198bb89e38
William McPherson <willmcpherson2@gmail.com>
2022-11-01 19:37:24 +1100

    Simplify testing

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -10,15 +10,10 @@ lazy_static! {
     };
 }
 
-pub fn init_idt() {
+pub fn init() {
     IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
-}
-
-#[test_case]
-fn test_breakpoint_exception() {
-    x86_64::instructions::interrupts::int3();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(os::test_runner)]
+#![feature(abi_x86_interrupt)]
+#![test_runner(test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+mod interrupts;
+mod serial;
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-use os::println;
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    os::init();
-
-    #[cfg(test)]
-    test_main();
-
-    println!("It did not crash!");
-
-    loop {}
-}
+use x86_64::instructions::port::Port;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -31,6 +22,55 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    use os::test_panic_handler;
-    test_panic_handler(info)
+    serial_println!("{}", info);
+    exit_qemu(QemuExitCode::Failed);
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    interrupts::init();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    serial_println!("Tests passed");
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn test_math() {
+    assert_eq!(1, 1);
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
