d28bb3591c0f0beae3220b1687ccd46421de4043
William McPherson <willmcpherson2@gmail.com>
2022-11-05 17:49:41 +1100

    Reorganise interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,20 +2,20 @@ use crate::{gdt::DOUBLE_FAULT_IST_INDEX, println};
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-lazy_static! {
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        unsafe {
-            idt.double_fault
-                .set_handler_fn(double_fault_handler)
-                .set_stack_index(DOUBLE_FAULT_IST_INDEX);
-        }
-        idt
-    };
-}
-
 pub fn init() {
+    lazy_static! {
+        static ref IDT: InterruptDescriptorTable = {
+            let mut idt = InterruptDescriptorTable::new();
+            idt.breakpoint.set_handler_fn(breakpoint_handler);
+            unsafe {
+                idt.double_fault
+                    .set_handler_fn(double_fault_handler)
+                    .set_stack_index(DOUBLE_FAULT_IST_INDEX);
+            }
+            idt
+        };
+    }
+
     IDT.load();
 }
 
